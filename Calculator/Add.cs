88d1a748d497b243d8ad57fa8cdae7927d36aaa6
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-27 11:59:26 +0200

    Add project files.

---
+++ Calculator/Add.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    class Math
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math caclulator = new Math();
+            Console.WriteLine("Velkommen til lommeregneren");
+            Console.WriteLine("Ønsker du at plusse to tal tryk: 1");
+            Console.WriteLine("Ønsker du at minus to tal tryk: 2");
+
+
+
+            string valg = Console.ReadLine();
+
+            if (valg == "1")
+            {
+                Console.WriteLine("Valg hvilken two tal du ønsker at plusse");
+
+
+                       Console.WriteLine("Valg dit første nummer");
+                     double num1 = Convert.ToDouble(Console.ReadLine());
+                      Console.WriteLine("Valg dit første nummer");
+                      double num2 = Convert.ToDouble(Console.ReadLine());
+
+                      Console.Write("{0} + {1} er: ", num1, num2);
+                       Console.WriteLine(caclulator.Add(num1, num2));
+            }
+            if (valg == "2")
+            {
+                Console.WriteLine("Valg hvilken two tal du ønsker at trække fra hinanden");
+
+
+                Console.WriteLine("Valg dit første nummer");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Valg dit første nummer");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("{0} - {1} er: ", num1, num2);
+                Console.WriteLine(caclulator.Substract(num1, num2));
+            }
+
+
+            //Valg af ønsket matematik
+
+
+            // Console.WriteLine(caclulator.Add(valg, 2));
+
+            //int tal1, tal2;
+
+
+
+
+
+
+        }
+    }
+}
